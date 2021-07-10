6c3190eb774d97b2d65d1fb7cf6daa600fbdc76e
Amirreza <amirreza.developer@gmail.com>
2021-07-10 18:34:57 +0430

    Add formatter for formatting order and generating response

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -4,13 +4,15 @@ namespace App\Http\Controllers;
 
 use Illuminate\Support\Arr;
 use App\Services\OrderService;
+use App\Formatters\OrderFormatter;
 use App\Http\Requests\OrderRequest;
 
 class OrderController
 {
     public function __construct(
         private OrderRequest $orderRequest,
-        private OrderService $orderService
+        private OrderService $orderService,
+        private OrderFormatter $orderFormatter
     )
     {}
 
@@ -19,5 +21,7 @@ class OrderController
         $orderData = $this->orderRequest->validated();
         $productIds = Arr::pluck($orderData['products'], 'id');
         $order = $this->orderService->order($productIds);
+
+        return response()->json($this->orderFormatter->format($order));
     }
 }
